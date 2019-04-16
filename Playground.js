79250462996121a109d16f4d77ea74676abce09d
Maxwell Montes Diaz <maxwell_montes@yahoo.com>
2019-04-16 15:43:30 -0700

    array functions for removing and adding

--- Playground.js
+++ Playground.js
@@ -1,9 +1,11 @@
-function wordBlanks(myNoun, myAdjective, myVerb, myAdverb)
-{
-    var result = "";
-    result += "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb + ".";
-    return result;
-}
+var ourArray = [1, 2, 3];
+ourArray.push(4); // adding to the end of the array
+ourArray.unshift(0); // adding to the beginning of the array
 
-console.log(wordBlanks("dog", "big", "ran", "quickly"));
-console.log(wordBlanks("bike", "slow", "flew", "slowly"));
+console.log(ourArray);
+
+var number = ourArray.pop(); // removes and returns last element in the array
+var otherNumber = ourArray.shift(); // removes and returns the first element in the array
+
+
+console.log(ourArray);
