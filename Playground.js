4b03db693d5be0138b42a89cbf4195e0a6652910
Maxwell Montes Diaz <maxwell_montes@yahoo.com>
2019-04-26 06:57:28 -0700

    Queue functionality through function call

--- Playground.js
+++ Playground.js
@@ -1,11 +1,11 @@
-var ourArray = [1, 2, 3];
-ourArray.push(4); // adding to the end of the array
-ourArray.unshift(0); // adding to the beginning of the array
+function nextInLine(arr, item)
+{
+    arr.push(item);
+    return arr.shift();
+}
 
-console.log(ourArray);
+var testArr = [1,2,3,4,5];
 
-var number = ourArray.pop(); // removes and returns last element in the array
-var otherNumber = ourArray.shift(); // removes and returns the first element in the array
-
-
-console.log(ourArray);
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
